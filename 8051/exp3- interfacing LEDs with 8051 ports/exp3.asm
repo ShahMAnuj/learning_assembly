9bf4d4f1feda1cf106bbab0ae65befb1e30e94bf
ShahMAnuj <0anujshah1@gmail.com>
2021-02-21 22:35:08 +0530

    Done with Expt 3!

--- 8051/exp3- interfacing LEDs with 8051 ports/exp3.asm
+++ 8051/exp3- interfacing LEDs with 8051 ports/exp3.asm
@@ -1,13 +1,15 @@
         ORG 0000H
 
-AGAIN:  MOV R3,#0FFH
-L3:     ACALL DELAY
-        DJNZ R3,L3
-        SJMP AGAIN
+MAIN:   MOV P0,#0FFH
+        ACALL DELAY
+        MOV P0,#00H
+		ACALL DELAY
+        JMP MAIN
 
 DELAY:  MOV R2,#0FFH
 L2:     MOV R1,#0FFH
 L1:     DJNZ R1,L1
         DJNZ R2,L2 
-	   
-        END
+		RET
+		
+		END
